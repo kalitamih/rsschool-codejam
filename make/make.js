981a8bb67fe05fef9e248ca21de509e63f6185cb
Kalita Mikhail <43149965+kalitamih@users.noreply.github.com>
2018-10-28 04:14:43 +0300

    perf: change arguments to spread operator

--- make/make.js
+++ make/make.js
@@ -1,9 +1,9 @@
-module.exports = function make() {
+module.exports = function make(...args) {
   const temporyArray = [];
-  for (let i = 0; i < arguments.length; i += 1) temporyArray.push(arguments[i]);
-  function inner() {
-    if (typeof arguments[0] === 'function') return temporyArray.reduce(arguments[0]);
-    for (let i = 0; i < arguments.length; i += 1) temporyArray.push(arguments[i]);
+  for (let i = 0; i < args.length; i += 1) temporyArray.push(args[i]);
+  function inner(...rest) {
+    if (typeof rest[0] === 'function') return temporyArray.reduce(rest[0]);
+    for (let i = 0; i < rest.length; i += 1) temporyArray.push(rest[i]);
     return inner;
   }
   return inner;
