1f8529256435b228afdd52945217a74ca308042e
Kalita Mikhail <43149965+kalitamih@users.noreply.github.com>
2018-10-28 03:52:47 +0300

    refactor: download files for recursion task

---
+++ recursion/recursion.js
@@ -0,0 +1,12 @@
+module.exports = function recursion(tree) {
+  const levels = [];
+  const createArray = (node, level) => {
+    if (!node) return null;
+    if (!levels[level]) levels[level] = [node.value];
+    else levels[level].push(node.value);
+    createArray(node.left, level + 1);
+    createArray(node.right, level + 1);
+  };
+  createArray(tree, 0);
+  return levels;
+};
