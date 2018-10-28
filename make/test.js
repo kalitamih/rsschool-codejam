b2c6ddd13a583fd9b19efd44a45c3f88cada8d23
Kalita Mikhail <mkalita1991@gmail.com>
2018-10-28 14:03:12 +0300

    docs: modify devDependencies and test the second time

--- make/test.js
+++ make/test.js
@@ -2,39 +2,32 @@ const assert = require('assert');
 const make = require('./make.js');
 
 function sum(a, b) {
-    return a + b;
-};
+  return a + b;
+}
 
 function multiply(a, b) {
-    return a * b;
-};
+  return a * b;
+}
 
 describe('make', () => {
   it('1', () => {
     const check = make(15)(34, 21, 666)(41)(sum);
     assert.deepEqual(check, 777);
-  });  
-  
- it('2', () => {
+  });
+  it('2', () => {
     const check = make(15)(34, 21, 666)(41)(multiply);
     assert.deepEqual(check, 292447260);
-  });  
-  
-  
-   it('3', () => {
+  });
+  it('3', () => {
     const check = make(15)(34, 21)(-1)(multiply);
     assert.deepEqual(check, -10710);
-  }); 
-  
-    it('4', () => {
+  });
+  it('4', () => {
     const check = make(15)(-34, 21)(-1)(sum);
     assert.deepEqual(check, 1);
-  }); 
-  
-   it('5', () => {
+  });
+  it('5', () => {
     const check = make(15)(21)(-1, 45, 665)(sum);
     assert.deepEqual(check, 745);
-  }); 
-  
-  
   });
+});
--- recursion/test.js
+++ recursion/test.js
@@ -1,34 +1,29 @@
 const assert = require('assert');
 const recursion = require('./recursion.js');
 
-let tree = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}};
+const tree = { value: 100, left: { value: 90, left: { value: 70 }, right: { value: 99 } }, right: { value: 120, left: { value: 110 }, right: { value: 130 } } };
 
-let tree2 = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110}}};
+const tree2 = { value: 100, left: { value: 90, left: { value: 70 }, right: { value: 99 } }, right: { value: 120, left: { value: 110 } } };
 
-let tree3 = {"value":100};
+const tree3 = { value: 100 };
 
-let tree4 = {"value":100,"left":{"value":87,"left":{"value":70},"right":{"value":90}},"right":{"value":120,"left":{"value":110},"right":{"value":1900}}};
+const tree4 = { value: 100, left: { value: 87, left: { value: 70 }, right: { value: 90 } }, right: { value: 120, left: { value: 110 }, right: { value: 1900 } } };
 
 describe('recursion', () => {
   it('1', () => {
     const check = recursion(tree);
-    assert.deepEqual(check, [[100], [90, 120], [70,99,110,130]]);
-  });  
-  
- it('2', () => {
+    assert.deepEqual(check, [[100], [90, 120], [70, 99, 110, 130]]);
+  });
+  it('2', () => {
     const check = recursion(tree2);
-    assert.deepEqual(check, [[100], [90, 120], [70,99,110]]);
-  });  
-  
-   it('3', () => {
+    assert.deepEqual(check, [[100], [90, 120], [70, 99, 110]]);
+  });
+  it('3', () => {
     const check = recursion(tree3);
     assert.deepEqual(check, [[100]]);
-  }); 
-    
+  });
   it('4', () => {
     const check = recursion(tree4);
-    assert.deepEqual(check, [[100], [87, 120], [70,90,110,1900]] );
-  }); 
-  
-  
+    assert.deepEqual(check, [[100], [87, 120], [70, 90, 110, 1900]]);
   });
+});
